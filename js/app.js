295bfbbccd374d0b287154fa485550bf7593489c
charlottesking <charlotte.is.king@gmail.com>
2026-09-16 11:38:23 -0500

    Lab 12

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
-console.log('app.js connected');
+let today = new DataTransfer().toLocaleDateString();
+console.log(today);
+
+ let myFavorite = {
+    name: 'Starbucks on University Drive',
+    category: 'coffee',
+    rating: 5,
+    notes: 'Great study spot with fast wifi',
+    dateAdded: today
+ };
+
+ console.log(myFavorite);
+ let displayText = myFavorite.name + '- Rating: ' + myFavorite.rating + '/5';
+ console.log(displayText);
+
+ console.log(typeof myFavorite.name);
+ console.log(typeof myFavorite.rating);
  
+ let placeName = myFavorite.name;
+ let rating = myFavorite.rating
+
+ console.log(placeName + '-' + rating + '/5');
+ console.log('⭐️'.repeat(rating) + ' ' + placeName);
